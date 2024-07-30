ae45c23174d6b0967d03666c9ad440584d1bcc8b
ImranNazirHussain <nh_immo@hotmail.com>
2024-07-30 22:52:44 +0300

    updating the file hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
 def fahrenheit_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5.0/9.0
     return celsius
+    
+    
+    number = 7
+even_number = odd_to_even(number)
+print(f"{number} converted to even is {even_number}")
+
+
